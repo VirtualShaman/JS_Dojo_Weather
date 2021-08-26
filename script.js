4d26e56f21359eaf1af5748a3dc9b2ba28f26599
Tyler Knight <TylerKnight2197@gmail.com>
2021-08-26 00:30:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function disappear() {
+    var x = document.getElementById("cookiebar");
+    x.remove();
+}
+
+// function imperial() {
+//     var x = document.querySelectorAll('.hot');
+//     x.innerHTML = Math.round((x.innerHTML*9/5)+32);
+//     return
+// }
